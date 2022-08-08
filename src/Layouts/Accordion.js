ccb6def3ee2fdc41115c7d56ad56191b7f289cf8
navidabdi <artsanaat@gmail.com>
2022-08-08 14:44:13 +0300

    fixed accordion bugs when it was opening and closing

--- src/Layouts/Accordion.js
+++ src/Layouts/Accordion.js
@@ -11,13 +11,13 @@ const Accordion = ({ title, content }) => {
   //   console.log("height: ", ref.current.clientHeight);
   // }, []);
   return (
-    <div key={title} className="border-b-2 border-blue-50">
-      <div className="flex items-center justify-between p-4 bg-[#fff] z-10">
+    <div key={title} className="border-b-2 border-blue-50 accordion">
+      <div
+        className="accordion-header flex items-center justify-between p-4 bg-[#fff] cursor-pointer"
+        onClick={() => setIsActiveAccordion(!isActiveAccordion)}
+      >
         <h4 className="">{title}</h4>
-        <button
-          onClick={() => setIsActiveAccordion(!isActiveAccordion)}
-          className="w-7 h-7 bg-blue-100 flex justify-center items-center rounded-full"
-        >
+        <button className="w-7 h-7 bg-blue-100 flex justify-center items-center rounded-full">
           <ChevronUpIcon
             className={`w-4 h-4 transition-all duration-500 ${
               isActiveAccordion ? "rotate-180" : ""
@@ -27,12 +27,8 @@ const Accordion = ({ title, content }) => {
       </div>
       <div
         ref={ref}
-        className={`p-4 transition-all duration-1000 relative z-0
-          ${
-            isActiveAccordion
-              ? `translate-y-[-110%] opacity-0 pointer-events-none`
-              : "translate-y-0 mt-0 opacity-100 pointer-events-auto"
-          }
+        className={`accordion-content p-4
+          ${isActiveAccordion ? `` : "active"}
         `}
         style={
           isActiveAccordion
