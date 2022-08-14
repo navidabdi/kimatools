e88eeb7d79d5406831c4809b753686a64978bb95
navidabdi <artsanaat@gmail.com>
2022-08-14 18:40:48 +0300

    add colorShadeOne and colorShadeTwo to the colorshadetools

---
+++ src/atoms/ColorShadeAtom.js
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+// Color Shade one
+export const colorShadeOneAtom = atom({
+  key: "colorShadeOneAtom",
+  default: "#eee",
+});
+// Color Shade two
+export const colorShadeTwoAtom = atom({
+  key: "colorShadeTwoAtom",
+  default: "#111",
+});
--- src/components/ColorShade/ColorShadeTools.js
+++ src/components/ColorShade/ColorShadeTools.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import {
+  colorShadeOneAtom,
+  colorShadeTwoAtom,
+} from "../../atoms/ColorShadeAtom";
+import { Accordion } from "../../Layouts";
+import { ColorPicker } from "../Controlers";
 
 const ColorShadeTools = () => {
-  return <div>ChromaTools</div>;
+  const [colorShadeOne, setColorShadeOne] = useRecoilState(colorShadeOneAtom);
+  const [colorShadeTwo, setColorShadeTwo] = useRecoilState(colorShadeTwoAtom);
+
+  const presetColors = [
+    "rgba(154, 205, 50,0.9)",
+    "rgba(54, 155, 50, 0.9)",
+    "rgba(154, 205, 255, 0.9)",
+    "rgba(25, 38, 255, 0.7)",
+    "rgba(1, 147, 255, 0.9)",
+    "rgba(255, 15, 255, 1)",
+    "rgba(200, 5, 200, 1)",
+    "rgba(255, 200, 12, 0.9)",
+    "rgba(200, 205, 255, 0.9)",
+    "rgba(180, 205, 145, 0.9)",
+  ];
+
+  return (
+    <>
+      <Accordion
+        title="Colors"
+        content={[
+          <ColorPicker
+            title="Color One"
+            color={colorShadeOne}
+            setColor={setColorShadeOne}
+            presetColors={presetColors}
+          />,
+          <ColorPicker
+            title="Color Two"
+            color={colorShadeOne}
+            setColor={colorShadeTwo}
+            presetColors={setColorShadeTwo}
+          />,
+        ]}
+      />
+    </>
+  );
 };
 
 export default ColorShadeTools;
