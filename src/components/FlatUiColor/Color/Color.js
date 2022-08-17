3d905ba13904657cc088a4e16d0517815d315cf3
navidabdi <artsanaat@gmail.com>
2022-08-17 14:53:01 +0300

    delete color.css

--- src/components/FlatUiColor/Color/Color.js
+++ src/components/FlatUiColor/Color/Color.js
@@ -4,7 +4,6 @@ import {
   menuTrigerAtom,
   soundTrigerAtom,
 } from "../../../atoms/FlatUiColor";
-import "./color.css";
 
 const Color = ({ color, setCopyTheColor, playSound }) => {
   const setCopyTriger = useSetRecoilState(copyTrigerAtom);
@@ -23,10 +22,12 @@ const Color = ({ color, setCopyTheColor, playSound }) => {
             setCopyTriger(false);
           }, 1000);
         }}
-        className="color"
+        className="color relative cursor-pointer overflow-hidden z-0 transition-all ease-in-out duration-200 before:content-['Copy'] before:absolute before:text-white before:font-semibold before:px-6 before:py-2 before:rounded-md before:border before:border-white before:left-[50%] before:top-[50%]  before:translate-x-[-50%] before:translate-y-[200%] before:transition-all before:duration-150 before:ease-in-out before:opacity-0 hover:before:opacity-100 hover:before:translate-y-[-50%]"
         style={{ backgroundColor: color.hex }}
       >
-        <p className="color__name">{color.name}</p>
+        <p className="color__name absolute right-3 bottom-3 text-white opacity-90 uppercase">
+          {color.name}
+        </p>
       </div>
     </>
   );
