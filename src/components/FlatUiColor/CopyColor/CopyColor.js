18f8630df2b37b4d83a4c43a7288f1fb0cccaffa
navidabdi <artsanaat@gmail.com>
2022-08-17 14:05:15 +0300

    update CopyColor and use tailwindcss on it.

--- src/components/FlatUiColor/CopyColor/CopyColor.js
+++ src/components/FlatUiColor/CopyColor/CopyColor.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./copyColor.css";
 import { useRecoilState } from "recoil";
 import { copyTrigerAtom } from "../../../atoms/FlatUiColor/copyTrigerAtom";
 const CopyColor = ({ copyTheColor }) => {
@@ -8,12 +7,22 @@ const CopyColor = ({ copyTheColor }) => {
   const scaleVal = copyTriger ? "scale(1)" : "scale(1.2)";
   return (
     <div
-      className="copycolor"
+      className="copycolor fixed inset-0 flex items-center justify-center text-white transition-opacity ease-in-out duration-300 z-50 pointer-events-none lefttoright"
       style={{ backgroundColor: copyTheColor, opacity: opacityVal }}
     >
-      <div className="data" style={{ transform: scaleVal }}>
-        <h2 style={{ textShadow: `2px 2px ${copyTheColor}` }}>Coped!</h2>
-        <p>{copyTheColor}</p>
+      <div
+        className="data w-full flex flex-col justify-center items-center transition-all duration-300 ease-in"
+        style={{ transform: scaleVal }}
+      >
+        <h2
+          className="text-[4rem] leading-[6rem] w-full text-center md:text-[7rem] md:leading-[10rem] bg-white/20"
+          style={{ textShadow: `2px 2px ${copyTheColor}` }}
+        >
+          Coped!
+        </h2>
+        <p className="text-[3.5rem] drop-shadow mt-10 font-semibold">
+          {copyTheColor}
+        </p>
       </div>
     </div>
   );
